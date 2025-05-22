42c4d81c93ef48330bfffb4d68aadd0a30d0d86e
Kevin Saukel <gf@saukels.de>
2025-05-22 15:52:24 +0200

    fix: box doesn't merge custom inline styles

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,11 +1,11 @@
 import type { BoxProps } from '../../style/Box';
 import { View } from 'react-native';
-import { applyBoxProps } from '../../style/Box';
 import { useTheme } from '../../hooks/useTheme';
+import { applyStyle } from '../../style';
 export const Box = (props: BoxProps) => {
     const theme = useTheme();
 
-    const { style, ...boxProps } = applyBoxProps(props, theme);
+    const { style, ...boxProps } = applyStyle(props, theme, {});
 
     return <View {...boxProps} style={style} />;
 };
